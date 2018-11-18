b11190ef2e0e5333cc70dfbee58cede7d12934ce
Lorenzo <lrnz.fontana@gmail.com>
2018-11-18 15:44:18 +0000

    Test results for Heroku

--- application.py
+++ application.py
@@ -44,7 +44,7 @@ def index():
 def result(sarg):
 
     # Get results from books
-    query = f"SELECT isbn, title, author, year FROM books WHERE isbn LIKE \'{sarg}%\' OR lower(author) LIKE \'%{sarg}%\' OR lower(title) LIKE \'%{sarg}%\' ORDER BY author, title"
+    query = f"SELECT isbn, title, author, year FROM books WHERE isbn LIKE '{sarg}%' OR lower(author) LIKE '%{sarg}%' OR lower(title) LIKE '%{sarg}%' ORDER BY author, title"
     query = text(query)
     results = db.execute(query).fetchall()
 
